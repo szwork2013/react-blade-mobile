b041cff490327bd34d49ac838567b95b281f86ba
OwlAford <1005476098@qq.com>
2016-12-19 21:39:45 +0800

    change 404 nofound page

--- src/components/NoFound.js
+++ src/components/NoFound.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import { message } from 'antd'
-
-const error = function () {
-  message.error('找不到该页面！')
-}
+import Result from 'antd-mobile/lib/result/index.web'
+import Toast from 'antd-mobile/lib/toast/index.web'
 
 export default class NotFound extends Component {
   static contextTypes = {
@@ -11,14 +9,20 @@ export default class NotFound extends Component {
   }
 
   componentWillMount() {
-    error()
+    Toast.info('加载失败!', 1);
     let timer = setTimeout(() => {
       this.context.router.replace('/')
       clearTimeout(timer)
-    }, 2000)
+    }, 3000)
   }
 
   render () {
-    return null
+    return (
+      <Result
+        imgUrl="https://zos.alipayobjects.com/rmsportal/NRzOqylcxEstLGf.png"
+        title="找不到该页面！"
+        message="未找到该页面，3秒后返回首页！"
+      />
+    )
   }
 }
